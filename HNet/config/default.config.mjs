490ac48f22384c2a8f61050056f5a84154503a4d
RexLee <62170324+RexLee0929@users.noreply.github.com>
2023-12-04 17:06:21 +0800

    Delete HNet directory

--- HNet/config/default.config.mjs
+++
@@ -1,4 +0,0 @@
-import dotEnv from 'dotenv';
-dotEnv.config();
-
-export default process.env
--- HNet/server/bs.default.mjs
+++
@@ -1,4 +0,0 @@
-import { createBareServer } from '@tomphttp/bare-server-node';
-const bareServer = createBareServer('/bare/');
-
-export default bareServer;
